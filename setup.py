779f29cd7e101263d54f1d9d033db77374787c0c
shivapbhusal <civabhusal@gmail.com>
2019-04-22 22:49:00 -0400

    add structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+        name='textpy',
+        version = '1.0',
+        packages =['textpy'],
+        url = '',
+        download_url='',
+        license ='',
+        author = 'Shiva Bhusal',
+        author_email = 'civabhusal@gmail.com',
+        package_data ={},
+        description = 'A Python package to extract structural features from a text'
+        )
---
+++ src/__init__.py
@@ -0,0 +1,17 @@
+__author__ = 'Shiva Bhusal'
+
+import textpy as tp
+
+def get_words(text):
+    '''
+    Converts roman number to Integer.
+    '''
+    parsed_text = tp.get_words(text)
+    return parsed_text
+
+def get_sentences(num):
+    '''
+    Converts integer to roman num.
+    '''
+    parsed_text = tp.get_words(text)
+    return parsed_text
