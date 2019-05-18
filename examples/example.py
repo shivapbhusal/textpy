3ebbee09c1158d8baa636e33216bcec2f2883fd6
shivapbhusal <civabhusal@gmail.com>
2019-05-18 12:35:50 -0400

    add exmaples

---
+++ examples/example.py
@@ -0,0 +1,7 @@
+"""
+This is an example module to test textpy
+"""
+import sys
+sys.path.append(r'/Users/shivabhusal/Documents/research/textpy/src')
+
+print(textpy.get_words('hello World'))
--- src/__init__.py
+++ src/__init__.py
@@ -6,12 +6,12 @@ def get_words(text):
     '''
     Converts roman number to Integer.
     '''
-    parsed_text = tp.get_words(text)
+    T = tp.TextPy()
+    parsed_text = T.get_words(text)
     return parsed_text
 
 def get_sentences(num):
     '''
     Converts integer to roman num.
     '''
-    parsed_text = tp.get_words(text)
-    return parsed_text
+    return 'Hello World'
